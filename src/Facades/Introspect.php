300207e0c1f5d24065c987dec052bf24217f0e36
james <james@designmynight.com>
2018-08-22 16:41:28 +0100

    doc: add method docs for ide autocomplete

--- src/Facades/Introspect.php
+++ src/Facades/Introspect.php
@@ -5,6 +5,21 @@ namespace DesignMyNight\Laravel\OAuth2\Facades;
 use DesignMyNight\Laravel\OAuth2\Introspect as IntrospectService;
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * Class Introspect
+ *
+ * @package DesignMyNight\Laravel\OAuth2\Facades
+ *
+ * @method static void mustHaveScopes(array $requiredScopes = [])
+ * @method static \DesignMyNight\Laravel\OAuth2\Intropect setUserDataKey(string $key)
+ * @method static \DesignMyNight\Laravel\OAuth2\Intropect setUserModelClass(string $class)
+ * @method static bool tokenIsActive()
+ * @method static bool tokenIsNotActive()
+ * @method static mixed|null getUser()
+ * @method static \Illuminate\Contracts\Auth\Authenticatable getUserModel()
+ * @method static string getUserModelClass()
+ * @method static \DesignMyNight\Laravel\OAuth2\Intropect verifyToken()
+ */
 class Introspect extends Facade
 {
     protected static function getFacadeAccessor()
